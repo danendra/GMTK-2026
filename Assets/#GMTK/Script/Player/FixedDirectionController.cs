e8f39e96cbbcd9c223e7eac122b84cd99b409089
Gde Danendra Arya Nugraha <danendra.anugraha@gmail.com>
2026-07-24 20:54:42 +0700

    [Add] Bullet Spawner controlling direction
    [Add] Spread and focus player shooter type

--- Assets/#GMTK/Script/Player/FixedDirectionController.cs
+++ Assets/#GMTK/Script/Player/FixedDirectionController.cs
@@ -11,6 +11,13 @@ namespace GMTK.Player
             directionInput = direction;
         }
 
+        public void SetDirection(Vector2 _direction)
+        {
+            direction = _direction;
+            
+            Awake();
+        }
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
